c56e2885eb2cd5204feaa7fafa33ddc2f0404733
Pavan <pavanadarsh2019@gmail.com>
2026-06-10 12:42:53 +0530

    not gate (needs changes)

--- not_gate.sv
+++ not_gate.sv
@@ -20,22 +20,22 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module not_gate(clk,reset,enable,seed1,seed2,A_state,B_state);
+//module not_gate(clk,reset,enable,seed1,seed2,A_state,B_state);
     
-    input logic clk,reset;
-    input logic [1:0] enable;
-    input logic [31:0] seed1,seed2;
+//    input logic clk,reset;
+//    input logic [1:0] enable;
+//    input logic [31:0] seed1,seed2;
     
-    output logic A_state,B_state;
+//    output logic A_state,B_state;
     
-    logic signed [7:0] NOT [0:0];
-    assign NOT[0] = -8'sd1;
+//    logic signed [7:0] NOT [0:0];
+//    assign NOT[0] = -8'sd1;
     
-    weighted_pbit #(.N(1))
-                    A(.clk(clk),.reset(reset),.enable(enable[0]),.m(B_state),.J(NOT),.h(8'd0),
-                    .seed(seed1),.out_bit(A_state));
-    weighted_pbit #(.N(1))
-                    B(.clk(clk),.reset(reset),.enable(enable[1]),.m(A_state),.J(NOT),.h(8'd0),
-                    .seed(seed2),.out_bit(B_state));
+//    weighted_pbit #(.N(1))
+//                    A(.clk(clk),.reset(reset),.enable(enable[0]),.m(B_state),.J(NOT),.h(8'd0),
+//                    .seed(seed1),.out_bit(A_state));
+//    weighted_pbit #(.N(1))
+//                    B(.clk(clk),.reset(reset),.enable(enable[1]),.m(A_state),.J(NOT),.h(8'd0),
+//                    .seed(seed2),.out_bit(B_state));
     
-endmodule
+//endmodule
