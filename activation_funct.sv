09e00a4a81ec1806a4f0987774f00f5b8b54d351
Pavan <pavanadarsh2019@gmail.com>
2026-06-12 12:35:01 +0530

    LUT made steeper

--- activation_funct.sv
+++ activation_funct.sv
@@ -27,22 +27,24 @@ module activation_funct(in,tanh_out);
     //simplified tanh LUT, can be made non-linear later
     always_comb begin
         case (in) 
-            -8: tanh_out = 32'h20000000;
-            -7: tanh_out = 32'h28000000;
-            -6: tanh_out = 32'h30000000;
-            -5: tanh_out = 32'h38000000;
-            -4: tanh_out = 32'h40000000;
-            -3: tanh_out = 32'h50000000;
-            -2: tanh_out = 32'h60000000;
-            -1: tanh_out = 32'h70000000;
-             0: tanh_out = 32'h80000000;
-             1: tanh_out = 32'h90000000;
-             2: tanh_out = 32'hA0000000;
-             3: tanh_out = 32'hB0000000;
-             4: tanh_out = 32'hC0000000;
-             5: tanh_out = 32'hC8000000;
-             6: tanh_out = 32'hD0000000;
-             7: tanh_out = 32'hD8000000;
+            -8: tanh_out = 32'h00010000;  // ~0.000015
+            -7: tanh_out = 32'h00080000;  // ~0.00012
+            -6: tanh_out = 32'h00200000;  // ~0.00049
+            -5: tanh_out = 32'h00800000;  // ~0.00195
+            -4: tanh_out = 32'h02000000;  // ~0.0078
+            -3: tanh_out = 32'h08000000;  // ~0.031
+            -2: tanh_out = 32'h20000000;  // 0.125
+            -1: tanh_out = 32'h50000000;  // 0.3125
+    
+             0: tanh_out = 32'h80000000;  // 0.5
+    
+             1: tanh_out = 32'hB0000000;  // 0.6875
+             2: tanh_out = 32'hE0000000;  // 0.875
+             3: tanh_out = 32'hF8000000;  // 0.96875
+             4: tanh_out = 32'hFE000000;  // 0.992
+             5: tanh_out = 32'hFF800000;  // 0.998
+             6: tanh_out = 32'hFFE00000;  // 0.9995
+             7: tanh_out = 32'hFFF80000;  // 0.9999
              
 //            -8: tanh_out = 32'h00100000;
 //            -7: tanh_out = 32'h00800000;
